86934eb0edd8f8c3b04b5c76463b61d786832534
bjin <bjin@bizjournals.com>
2019-12-31 13:57:00 -0500

    Update hamburger menu

--- src/components/Navigation/index.vue
+++ src/components/Navigation/index.vue
@@ -1,34 +1,41 @@
 <template>
   <div id="nav">
-    <div class="nav-btn__wrapper" v-show="showMobileMenu">
-      <router-link class="nav__btn" to="/">HOME</router-link>
-      <router-link class="nav__btn" to="/about">MENU</router-link>
-      <a target="_blank" class="nav__btn" href="https://ordering.chownow.com/order/4388/locations">
-        ONLINE ORDERING
-      </a>
-      <router-link class="nav__btn" to="/about">WHISKY</router-link>
-      <router-link class="nav__btn" to="/about">GALLERY</router-link>
-      <router-link class="nav__btn" to="/about">PRESS</router-link>
-    </div>
+    <transition name="flip">
+      <div class="nav-btn__wrapper" v-show="showMobileMenu">
+        <router-link class="nav__btn" to="/">HOME</router-link>
+        <router-link class="nav__btn" to="/about">MENU</router-link>
+        <a
+          target="_blank"
+          class="nav__btn"
+          href="https://ordering.chownow.com/order/4388/locations"
+        >
+          ONLINE ORDERING
+        </a>
+        <router-link class="nav__btn" to="/about">WHISKY</router-link>
+        <router-link class="nav__btn" to="/about">GALLERY</router-link>
+        <router-link class="nav__btn" to="/about">PRESS</router-link>
+      </div>
+    </transition>
+    <!-- eslint-disable  -->
 
     <svg
-      class="ham hamRotate ham1"
+      class="ham hamRotate ham4"
       viewBox="0 0 100 100"
-      width="80"
+      width="60"
       ref="ham"
       @click="handleHamMenuClick"
     >
-      <!-- eslint-disable  -->
       <path
         class="line top"
-        d="m 30,33 h 40 c 0,0 9.044436,-0.654587 9.044436,-8.508902 0,-7.854315 -8.024349,-11.958003 -14.89975,-10.85914 -6.875401,1.098863 -13.637059,4.171617 -13.637059,16.368042 v 40"
+        d="m 70,33 h -40 c 0,0 -8.5,-0.149796 -8.5,8.5 0,8.649796 8.5,8.5 8.5,8.5 h 20 v -20"
       />
-      <path class="line middle" d="m 30,50 h 40" />
+      <path class="line middle" d="m 70,50 h -40" />
       <path
         class="line bottom"
-        d="m 30,67 h 40 c 12.796276,0 15.357889,-11.717785 15.357889,-26.851538 0,-15.133752 -4.786586,-27.274118 -16.667516,-27.274118 -11.88093,0 -18.499247,6.994427 -18.435284,17.125656 l 0.252538,40"
+        d="m 30,67 h 40 c 0,0 8.5,0.149796 8.5,-8.5 0,-8.649796 -8.5,-8.5 -8.5,-8.5 h -20 v 20"
       />
     </svg>
+    <!-- eslint-enable  -->
   </div>
 </template>
 
